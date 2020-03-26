f6e7d532fcba383defffe2a4d6685f5b82bdc625
HideBa <baba.papa1120.ba@gmail.com>
2020-03-26 10:18:31 +0900

    static pages test

---
+++ sample_app/test/helpers/application_helper_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+# require 'test_helper'
+
+# class ApplicationHelperTest < ActionView::TestCase
+#   test "full title helper" do
+#     assert_equal full_title, full_title("Contact")
+#     assert_equal full_title("Help"),
+#   end
+# end

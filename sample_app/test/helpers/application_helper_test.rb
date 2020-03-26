30b3922af42853cc001eef7200eb931e538e8891
HideBa <baba.papa1120.ba@gmail.com>
2020-03-26 11:59:40 +0900

    add user model and test

--- sample_app/test/helpers/application_helper_test.rb
+++ sample_app/test/helpers/application_helper_test.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
-# require 'test_helper'
 
-# class ApplicationHelperTest < ActionView::TestCase
-#   test "full title helper" do
-#     assert_equal full_title, full_title("Contact")
-#     assert_equal full_title("Help"),
-#   end
-# end
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test 'full title helper' do
+    p '---------' + full_title
+    assert_equal full_title, full_title('')
+    assert_equal full_title('Help'), 'Help | Ruby on Rails Tutorial Sample App'
+  end
+end
